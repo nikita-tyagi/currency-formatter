5394ce18238cfd8dd2e7ed8493beed4fcb9ae645
nikita.tyagi <nikita.tyagi@web.com>
2020-07-23 18:32:03 +0530

    vanilla layer commit

---
+++ src/main/java/com/currency/formatter/currencyformatter/CurrencyFormatterApplication.java
@@ -0,0 +1,13 @@
+package com.currency.formatter.currencyformatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyFormatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyFormatterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/currency/formatter/currencyformatter/CurrencyFormatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currency.formatter.currencyformatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyFormatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
